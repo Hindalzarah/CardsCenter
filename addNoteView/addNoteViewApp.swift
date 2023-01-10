d0d18fa480ca99c1c624df8315417f9ea340d330
Hind <hindalzarah@outlook.com>
2023-01-10 10:27:54 +0300

    Initial Commit

---
+++ addNoteView/addNoteViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  addNoteViewApp.swift
+//  addNoteView
+//
+//  Created by Hind on 17/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct addNoteViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
